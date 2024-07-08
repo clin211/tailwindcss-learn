1db95c39a06725fdae9dd799df840bb3e9e98acb
Forest <767425412lin@gmail.com>
2024-07-08 18:19:18 +0800

    feat: 添加首页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,48 @@
-import Link from 'next/link';
+import HoverEffect from '@/components/HoverEffectCard';
 
-export default function Home() {
+const projects = [
+    {
+        title: 'Stripe',
+        description:
+            'A technology company that builds economic infrastructure for the internet.',
+        link: 'https://stripe.com',
+    },
+    {
+        title: 'Netflix',
+        description:
+            'A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.',
+        link: 'https://netflix.com',
+    },
+    {
+        title: 'Google',
+        description:
+            'A multinational technology company that specializes in Internet-related services and products.',
+        link: 'https://google.com',
+    },
+    {
+        title: 'Meta',
+        description:
+            "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+        link: 'https://meta.com',
+    },
+    {
+        title: 'Amazon',
+        description:
+            'A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.',
+        link: 'https://amazon.com',
+    },
+    {
+        title: 'Microsoft',
+        description:
+            'A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.',
+        link: 'https://microsoft.com',
+    },
+];
+
+export default function Page() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Link
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                href="/login/one">
-                login one
-            </Link>
-            <Link
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                href="/login/two">
-                login two
-            </Link>
-            <Link
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                href="/login/three">
-                login three
-            </Link>
-            <Link
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                href="/placeholders-and-vanish-input">
-                Placeholders And Vanish Input
-            </Link>
-        </main>
+        <div className="max-w-5xl mx-auto px-8">
+            <HoverEffect items={projects} />
+        </div>
     );
 }
